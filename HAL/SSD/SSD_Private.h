160af6c2970850cf82bf0343cb77e3254faaca8d
Nada Hassan <engrnada@users.noreply.github.com>
2023-11-13 00:42:02 +0200

    Create SSD_Private.h

---
+++ HAL/SSD/SSD_Private.h
@@ -0,0 +1,14 @@
+/*
+ * SSD_Private.h
+ *
+ *  Created on: Nov 4, 2023
+ *      Author: Nada
+ */
+
+#ifndef SSD_PRIVATE_H_
+#define SSD_PRIVATE_H_
+
+#define SSD_Arr {0b00111111 , 0b00000110 , 0b01011011 , 0b01001111 , 0b01100110 , 0b01101101 , 0b01111101 , 0b00100111 , 0b01111111 , 0b01101111}
+
+
+#endif /* SSD_PRIVATE_H_ */
